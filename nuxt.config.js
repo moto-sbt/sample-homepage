b2a7f7e5f79ac0ee26a86cde5ae80a2fbf7927bb
moto-sbt <moto1220.sbt@outlook.jp>
2022-01-31 21:00:41 +0900

    feat: サーバーにアップできるように設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,5 +47,14 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+
+  router: {
+    base: '/motonori/' // ここをサブディレクトリのurl名にする
+   },
+    head: {
+      base: {
+        href: 'router.base'
+      },
+    }
 }
--- pages/inquiry/index.vue
+++ pages/inquiry/index.vue
@@ -1,11 +1,20 @@
 <template>
-  <h1>お問い合わせ</h1>
+  <div>
+    <h1>お問い合わせ</h1>
+    <button @click="buttonClick">app</button>
+  </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import Vue from 'vue'
 
-}
+export default Vue.extend({
+  methods: {
+    buttonClick(): void {
+      console.log("test")
+    }
+  }
+})
 </script>
 
 <style>
